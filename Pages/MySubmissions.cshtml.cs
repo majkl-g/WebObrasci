6a8e97cb6f8a9ed46dce750d918700f4ba6601b9
mihael.glavicic@gmail.com <mihael.glavicic@gmail.com>
2026-06-08 20:58:07 +0200

    Added MySubmissions

---
+++ Pages/MySubmissions.cshtml.cs
@@ -0,0 +1,39 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebObrasci1.Data;
+using WebObrasci1.Models;
+
+namespace WebObrasci1.Pages
+{
+    [Authorize]
+    public class MySubmissionsModel : PageModel
+    {
+        private readonly AppDbContext _context;
+        public MySubmissionsModel(AppDbContext context) => _context = context;
+
+        public List<FormSubmission> Submissions { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            var externalId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                           ?? User.FindFirst("sub")?.Value;
+
+            if (string.IsNullOrWhiteSpace(externalId))
+                return;
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.ExternalId == externalId);
+
+            if (user == null)
+                return;
+
+            Submissions = await _context.FormSubmissions
+                .Include(s => s.Form)
+                .Where(s => s.UserId == user.Id)
+                .OrderByDescending(s => s.SubmittedAt)
+                .ToListAsync();
+        }
+    }
+}
