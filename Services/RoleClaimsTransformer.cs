29c0b3ec3f735b9a9320d1efb3b98891a62a1f34
mihael.glavicic@gmail.com <mihael.glavicic@gmail.com>
2026-06-14 17:06:43 +0200

    aaiedu compatible claims

--- Services/RoleClaimsTransformer.cs
+++ Services/RoleClaimsTransformer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
+using System.Text.Json;
 using WebObrasci1.Settings;
 
 namespace WebObrasci1.Services
@@ -18,14 +19,37 @@ namespace WebObrasci1.Services
         {
             var identity = (ClaimsIdentity)principal.Identity!;
 
-            if (principal.IsInRole(_roleSettings.Value.StudentRole) && !principal.IsInRole(Role.Student))
+            if (HasJsonClaimValue(identity, _roleSettings.Value.StudentClaim, _roleSettings.Value.StudentClaimValue))
                 identity.AddClaim(new Claim(ClaimTypes.Role, Role.Student));
-            else if (principal.IsInRole(_roleSettings.Value.ProfesorRole) && !principal.IsInRole(Role.Profesor))
+            else if (HasJsonClaimValue(identity, _roleSettings.Value.ProfesorClaim, _roleSettings.Value.ProfesorClaimValue))
                 identity.AddClaim(new Claim(ClaimTypes.Role, Role.Profesor));
-            else if (principal.IsInRole(_roleSettings.Value.AdminRole) && !principal.IsInRole(Role.Admin))
+            else if (HasJsonClaimValue(identity, _roleSettings.Value.AdminClaim, _roleSettings.Value.AdminClaimValue))
                 identity.AddClaim(new Claim(ClaimTypes.Role, Role.Admin));
 
             return Task.FromResult(principal);
+        }
+
+        private bool HasJsonClaimValue(ClaimsIdentity identity, string claimName, string targetValue)
+        {
+            var claim = identity.Claims.FirstOrDefault(c => c.Type == claimName);
+            if (string.IsNullOrEmpty(claim?.Value) == false)
+            {
+                try
+                {
+                    var values = JsonSerializer.Deserialize<string[]>(claim.Value);
+                    if (values != null && values.Contains(targetValue))
+                        return true;
+                }
+                catch (JsonException) 
+                {
+                    //skip this claim
+                }
+
+                //fallback if not json array:
+                if (claim.Value.Equals(targetValue))
+                    return true;
+            }
+            return false;
         }
     }
 }
--- Settings/UserSettings.cs
+++ Settings/UserSettings.cs
@@ -3,10 +3,14 @@
     public class UserSettings
     {
         public string IdClaim { get; set; } = "sid";
-        public string StudentRole { get; set; } = "student";
-        public string ProfesorRole { get; set; } = "profesor";
-        public string AdminRole { get; set; } = "admin";
         public string UsernameClaim { get; set; } = "username";
         public string EmailClaim { get; set; } = "email";
+        
+        public string StudentClaim { get; set; } = "hrEduPersonAffiliation";
+        public string StudentClaimValue { get; set; } = "student";
+        public string ProfesorClaim { get; set; } = "hrEduPersonAffiliation";
+        public string ProfesorClaimValue { get; set; } = "djelatnik";
+        public string AdminClaim { get; set; } = "Admin";
+        public string AdminClaimValue { get; set; } = "Admin";
     }
 }
